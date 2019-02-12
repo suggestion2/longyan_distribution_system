a2208df308b59208e8a639c0fba7284c90a3e658
Îâ <695067018@qq.com>
2019-02-13 00:20:29 +0800

    正则

--- src/main/java/com/longyan/distribution/constants/SystemParamsConstants.java
+++ src/main/java/com/longyan/distribution/constants/SystemParamsConstants.java
@@ -41,12 +41,12 @@ public class SystemParamsConstants {
     public static final String COMMONOILCHARGE_REGEX  = "^[0-9]+(\\.[0-9]{1,2})?$";//普通会员充值油钻的充值折扣
     public static final String VIPREOILCHARGE_REGEX  = "^[0-9]+(\\.[0-9]{1,2})?$";//VIP充值油钻的充值折扣
     public static final String PARTNEOILRRECHARGE_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//合伙人充值油钻的充值折扣
-    public static final String VIPINVITENUM_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//VIP邀请x人升级为合伙人
+    public static final String VIPINVITENUM_REGEX = "^[0-9]*$";//VIP邀请x人升级为合伙人
     public static final String COINCASH_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//钢镚提现手续费费率
     public static final String BUSINESSGOLDCASH_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//商户金币提现手续费费率
     public static final String COINCHANGEGOLD_REGEX= "^[0-9]+(\\.[0-9]{1,2})?$";//钢镚转金币手续费费率
     public static final String COINCHANGEOIL_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//钢镚转油钻的手续费费率
-    public static final String OILPRICE_REGEX = "/6";//当日油价,展示在前台页面，仅做展示
+    public static final String OILPRICE_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//当日油价,展示在前台页面，仅做展示
     public static final String INVITECOMMONBECOMEVIPCOIN_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//合伙人或VIP邀请普通会员,普通会员升级vip，vip获得的钢镚百分比
     public static final String INVITEVIPINVITECOMMONCOIN_REGEX = "^[0-9]+(\\.[0-9]{1,2})?$";//合伙人邀请vip，vip邀请普通会员,普通会员升级vip，合伙人获得的钢蹦百分比
     public static final String VIPCARD_REGEX = "^[0-9]*$";//vip会员卡
